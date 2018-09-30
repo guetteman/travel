53d8ac37fd20dc4e50e0c70fdb5dbc649b3236a8
Luis Güette <guetteluis@gmail.com>
2018-09-30 18:44:01 -0400

    icons library added, delete button changed

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,7 @@
 import React from 'react'; 
-import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
+import { Modal, View, Image, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const placeDetail = props => {
     let modalContent = null;
@@ -25,11 +27,18 @@ const placeDetail = props => {
             <View style={styles.modalContainer}>
                 {modalContent}
                 <View>
-                    <Button 
-                        title="Delete" 
-                        onPress={props.onItemDeleted} 
-                        color="red"
-                    />
+                    <View style={styles.deleteButtonContainer}>
+                        <TouchableOpacity onPress={props.onItemDeleted}>
+                            <View style={styles.deleteButton}>
+                                <Icon 
+                                    size={20} 
+                                    name="trash"
+                                    color="white"
+                                />
+                                <Text style={styles.deleteButtonText} >Delete</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                     <Button 
                         title="Close" 
                         onPress={props.onModalClosed} 
@@ -53,6 +62,24 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontSize: 28,
         color: "#546E7A"
+    },
+    deleteButton: {
+        padding: 15,
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor:"#EF5350",
+        borderRadius: 30
+    },
+    deleteButtonContainer: {
+        width: "100%",
+        alignItems: "center",
+        marginTop: 10,
+        marginBottom: 10
+    },
+    deleteButtonText: {
+        marginLeft: 10,
+        color: "white",
+        fontSize: 20
     }
 }); 
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,19 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import placesReducers from './reducers/places';
 
 const rootReducer = combineReducers({
     places: placesReducers
-})
+});
+
+let composerEnhancers = compose;
+
+if (__DEV__) {
+    composerEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
 
 const configureStore = () => {
-    return createStore(rootReducer);
-}
+    return createStore(rootReducer, composerEnhancers());
+};
 
 export default configureStore;
