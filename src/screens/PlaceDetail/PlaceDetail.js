e4cc37826a5b1b5e9e14bb491f82455c237d8eba
Luis Güette <guetteluis@gmail.com>
2018-10-02 21:34:41 -0400

    PlaceDetail refactored

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, ScrollView, Image, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
@@ -7,6 +7,26 @@ import { deletePlace } from '../../store/actions';
 
 class PlaceDetail extends Component {
     
+    state = {
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+    }
+
+    constructor(props) {
+        super(props);
+
+        Dimensions.addEventListener("change", this.updateStyles);
+    }
+
+    componentWillUnmount = () => {
+        Dimensions.removeEventListener("change", this.updateStyles);
+    }
+
+    updateStyles = (dimensions) => {
+        this.setState({
+            viewMode: dimensions.window.height > 500 ? "portrait" : "landscape"
+        });
+    }
+
     placeDeletedHandler = () => {
         this.props.onDeletePlace(this.props.selectedPlace.key);
         this.props.navigator.pop();
@@ -14,29 +34,43 @@ class PlaceDetail extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <View>
-                    <Image 
-                        source={this.props.selectedPlace.image} 
-                        style={styles.image}
-                    />
-                    <Text style={styles.name}>{this.props.selectedPlace.name}</Text>
-                </View>
-                <View>
-                    <View style={styles.deleteButtonContainer}>
-                        <TouchableOpacity onPress={this.placeDeletedHandler}>
-                            <View style={styles.deleteButton}>
-                                <Icon 
-                                    size={20} 
-                                    name="trash"
-                                    color="white"
-                                />
-                                <Text style={styles.deleteButtonText} >Delete</Text>
-                            </View>
-                        </TouchableOpacity>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View 
+                        style={
+                            this.state.viewMode === "portrait" 
+                            ? styles.portraitInfoContainer 
+                            : styles.landscapeInfoContainer
+                        }
+                    >
+                        <Image 
+                            source={this.props.selectedPlace.image} 
+                            style={
+                                this.state.viewMode === "portrait" 
+                                ? styles.portraitImage 
+                                : styles.landscapeImage
+                            }
+                        />
+                        <Text style={styles.name}>
+                            {this.props.selectedPlace.name}
+                        </Text>
                     </View>
-                </View>
-            </View>
+                    <View>
+                        <View style={styles.deleteButtonContainer}>
+                            <TouchableOpacity onPress={this.placeDeletedHandler}>
+                                <View style={styles.deleteButton}>
+                                    <Icon 
+                                        size={20} 
+                                        name="trash"
+                                        color="white"
+                                    />
+                                    <Text style={styles.deleteButtonText}>Delete</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>   
+            </ScrollView>
         );
     };
 };
@@ -45,9 +79,20 @@ const styles = StyleSheet.create({
     container: {
         margin: 22
     },
-    image: {
+    portraitInfoContainer: {
+        flexDirection: "column"
+    },
+    landscapeInfoContainer: {
+        flexDirection: "row"
+    },
+    portraitImage: {
         width: "100%",
         height: 200
+    },
+    landscapeImage: {
+        width: "50%",
+        height: 150,
+        marginRight: 10
     },
     name: {
         fontWeight: "bold",
