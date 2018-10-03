7785ba44c66635d01d54745448ded393ab5bdcbc
Luis Güette <guetteluis@gmail.com>
2018-10-02 20:11:37 -0400

    Buttons refactored for each platform

--- src/components/UI/DefaultButton/DefaultButton.js
+++ src/components/UI/DefaultButton/DefaultButton.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, TouchableNativeFeedback, Platform, View, StyleSheet } from 'react-native';
 
 const defaultButton = props => {
+    const content = (
+        <View style={[styles.button, {backgroundColor: props.color}]}>
+            <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+    );
+
+    if (Platform.OS === "android") {
+        return (
+            <TouchableNativeFeedback onPress={props.onPress}>
+                {content}
+            </TouchableNativeFeedback>
+        );
+    }
+
     return (
         <TouchableOpacity onPress={props.onPress}>
-            <View style={[styles.button, {backgroundColor: props.color}]}>
-                <Text style={styles.buttonText}>{props.children}</Text>
-            </View>
+            {content}
         </TouchableOpacity>
     );
 }
@@ -20,7 +32,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         backgroundColor:"#EF5350",
-        borderRadius: 25
+        borderRadius: 10
     },
     buttonText: {
         color: "white",
