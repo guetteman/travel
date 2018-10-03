7b52c99780677107d911deb7402fc2facfdd5566
Luis Güette <guetteluis@gmail.com>
2018-10-02 20:56:48 -0400

    Login page refactored to be responsive

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, ImageBackground, StyleSheet } from 'react-native';
+import { View, ImageBackground, StyleSheet, Dimensions } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import HeadingText from '../../components/UI/HeadingText/HeadingText'
@@ -9,26 +9,63 @@ import DefaultButton from '../../components/UI/DefaultButton/DefaultButton';
 import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
+    
+    state = {
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+    }
+    
+    constructor(props) {
+        super(props);
+
+        Dimensions.addEventListener("change", (dimensions) => {
+            this.setState({
+                viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+            });
+        });
+    }
+
     loginHandler = () => {
         startMainTabs();
     }
 
     render () {
+        let headingText = null;
+
+        if (this.state.viewMode === "portrait") {
+            headingText = (
+                <HeadingText>Please Log In</HeadingText>
+            );
+        }
+
         return (
             <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
                 <View style={styles.container}>
-                        <HeadingText>Please Log In</HeadingText>
+                        {headingText}
                         <View style={styles.buttonContainer}>
                             <DefaultButton color="#0288D1">Switch to Login</DefaultButton>
                         </View>
                         <View style={styles.inputContainer} >
                             <DefaultInput placeholder="Your E-Mail Address" />
                         </View>
-                        <View style={styles.inputContainer} >
-                            <DefaultInput placeholder="Password" />
-                        </View>
-                        <View style={styles.inputContainer} >
-                            <DefaultInput placeholder="Confirm Password" />
+                        <View 
+                            style={
+                                this.state.viewMode === "portrait" 
+                                ? styles.portraitPasswordsContainer 
+                                : styles.landscapePasswordsContainer
+                            } 
+                        >
+                            <View 
+                                style={
+                                    this.state.viewMode === "portrait" 
+                                    ? styles.portraitPasswordWrapper 
+                                    : styles.landscapePasswordWrapper
+                                } 
+                            >
+                                <DefaultInput placeholder="Password" />
+                            </View>
+                            <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper} >
+                                <DefaultInput placeholder="Confirm Password" />
+                            </View>
                         </View>
                         <View style={styles.buttonContainer}>
                             <DefaultButton color="#0288D1" onPress={this.loginHandler}>Login</DefaultButton>
@@ -56,6 +93,24 @@ const styles = StyleSheet.create({
     },
     buttonContainer: {
         marginTop: 10
+    },
+    landscapePasswordsContainer: {
+        width: "80%",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    portraitPasswordsContainer: {
+        width: "80%",
+        flexDirection: "column",
+        justifyContent: "flex-start"
+    },
+    landscapePasswordWrapper: {
+        marginTop: 10,
+        width: "48%"
+    },
+    portraitPasswordWrapper: {
+        marginTop: 10,
+        width: "100%"
     }
 })
 
